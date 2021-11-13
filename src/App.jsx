6d3409fe9ee8f35580b71d5ef0d77e930d1878ed
Shin H <nobunegi13@gmail.com>
2021-11-13 12:56:40 +0900

    [feat]必要なHTML要素を記載した。

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,27 @@
-import React from "react";
+import React from 'react';
+import trashCan from "./images/trash_can.svg";
 
 export const App = () => {
-  return null;
+
+  return (
+    <>
+      <p>Todo List</p>
+      <div>
+        <p>ADD TODO</p>
+        <input type="text" placeholder="New Todo" />
+      </div>
+      <div>
+        <ul>
+          <div>
+            <li>Todo1</li>
+            <input type="image" src={trashCan} alt="削除" />
+          </div>
+          <div>
+            <li>Todo2</li>
+            <input type="image" src={trashCan} alt="削除" />
+          </div>
+        </ul>
+      </div>
+    </>
+  );
 };
