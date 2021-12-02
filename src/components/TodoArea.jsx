df1d9af1d2d62a55f98c4ccf1a5418f22f26e383
Shin H <nobunegi13@gmail.com>
2021-12-02 20:17:30 +0900

    [feat]taodo検索機能を実装した。

--- src/components/TodoArea.jsx
+++ src/components/TodoArea.jsx
@@ -3,24 +3,24 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faTrashAlt } from "@fortawesome/free-regular-svg-icons";
 
 export const TodoArea = (props) => {
-  const { todos, onClickDelete } = props;
+  const { todos, onClickDelete} = props;
 
   return (
     <>
       <ul>
-        {todos.map((todo, index) => {
+        {todos.map((todo) => {
           return (
-            <div key={todo} className="todo-list">
-              <li className="list-low">
-                {todo}
+            <div key={todo.id} className="todo-list">
+              {todo.todoShowFlag && <li className="list-low">
+                {todo.title}
                 {/* 引数を渡した関数をonClickに指定すると画面がレンダリングされた際にonClickイベントが発火してしまうためアロー関数にして指定する */}
                 <FontAwesomeIcon
-                  onClick={() => onClickDelete(index)}
+                  onClick={() => onClickDelete(todo.id)}
                   icon={faTrashAlt}
                   alt="削除"
                   className="icon"
                 />
-              </li>
+              </li> }
             </div>
           );
         })}
